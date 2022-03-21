7184561413b2b84c937f79bcabcef69a04438641
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-03-21 17:58:23 +0300

    style:add toggle and backround feat to buttons

--- script/script.js
+++ script/script.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 //user logic
-$(#submitbtn).click(()=>{
-
-
+$("#place-order").click(()=>{
+  $(".order-table").toggle();
+  $("#place-order").toggle();
+  $("#add-another").toggle();
+});
+$("#place-order").click(()=>{
 
 });
 
@@ -15,7 +18,21 @@ $(#submitbtn).click(()=>{
   let size = $('.size-of-pizza option:selected').value();
   let topping = $('.topping option:selected').value();
   let delivery = $('.delivery option:selected').value();
-  
+  let orderNumber = 1;
+  let totalPrice = 0;
+
+
+
+
+
+  //constructors
+  function Pizza (crust, size, topping, delivery, number){
+    this.crust = crust;
+    this.size = size;
+    this.topping = topping;
+    this.delivery = delivery;
+    this.number = number;
+  }
 
 
 
