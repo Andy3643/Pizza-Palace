68686acafdd99afcc95486e88f3d40648a0d6524
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-03-21 16:51:55 +0300

    feat:add data collectors to form

--- script/script.js
+++ script/script.js
@@ -1,43 +1,59 @@
 $(document).ready(function(){
+//user logic
+$(#submitbtn).click(()=>{
+
+
+
+});
 
 
 
 
+//busssiness logic
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  let crust = $('.crust-type option:selected').value();
+  let size = $('.size-of-pizza option:selected').value();
+  let topping = $('.topping option:selected').value();
+  let delivery = $('.delivery option:selected').value();
   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
