7a101dc5b2b02cf28d8b73e3f109a1e5756689d8
Moringa School <joshua.mulonga@student.moringaschool.com>
2022-03-22 08:35:18 +0300

    feat:declare variable values

--- script/script.js
+++ script/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 //user logic
 $("#place-order").click(()=>{
   $(".order-table").toggle();
-  $("#place-order").toggle();
-  $("#add-another").toggle();
+  // $("#place-order").toggle();
+  // $("#add-another").toggle();
 });
 $("#place-order").click(()=>{
 
@@ -14,12 +14,9 @@ $("#place-order").click(()=>{
 
 //busssiness logic
 
-  let crust = $('.crust-type option:selected').value();
-  let size = $('.size-of-pizza option:selected').value();
-  let topping = $('.topping option:selected').value();
-  let delivery = $('.delivery option:selected').value();
-  let orderNumber = 1;
-  let totalPrice = 0;
+  
+  // let totalPrice = 0;
+  // console.log(crust,size,topping,delivery);
 
 
 
@@ -28,20 +25,20 @@ $("#place-order").click(()=>{
   //constructors
   $("#place-order").click(()=>{
 
-  
-    function Pizza (crust, size, topping, delivery, number){
+    let crust = $('.crust-type option:selected').val();
+    let size = $('.size-of-pizza option:selected').val();
+    let topping = $('.topping option:selected').val();
+    let delivery = $('.delivery option:selected').val();
+
+    function Pizza (crust, size, topping, delivery){
       this.crust = crust;
       this.size = size;
       this.topping = topping;
       this.delivery = delivery;
-      this.number = number;
+      // this.number = number;
     }
 
-    $("#crust-type").html($(".crust-type option:selected").text() + " - " + crust);
-    $("#crust-size").html($(".size-of-pizza option:selected").text() + " - " + size);
-    $("#topping").html($(".topping option:selected").text() + " - " + topping);
-    $("#total").html("total");
-
+    
   });
 
 
